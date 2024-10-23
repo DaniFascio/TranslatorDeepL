4f4531e5f0224b31630a23e37bd9e965bc0cf25d
danielefasciani <fascianidaniele@gmail.com>
2024-10-23 15:54:25 +0200

    Languages.java renamed

--- src/main/java/com/deepl/translator/utils/Languages.java
+++ src/main/java/com/deepl/translator/utils/Languages.java
@@ -6,42 +6,40 @@ import lombok.*;
 @AllArgsConstructor
 public enum Languages {
 
-    ARABO("AR", "Arabo"),
-    BULGARO("BG", "Bulgaro"),
-    CECO("CS", "Ceco"),
-    CINESE("ZH-HANT", "Cinese"),
-    COREANO("KO", "Coreano"),
-    DANESE("DA", "Danese"),
-    ESTONE("ET", "Estone"),
-    FINLANDESE("FI", "Finlandese"),
-    FRANCESE("FR", "Francese"),
-    GIAPPONESE("JA", "Giapponese"),
-    GRECO("EL", "Greco"),
-    INDONESIANO("ID", "Indonesiano"),
-    INGLESE("EN-GB", "Inglese"),
-    ITALIANO("IT", "Italiano"),
-    LETTONE("LV", "Lettone"),
-    LITUANO("LT", "Lituano"),
-    NORVEGESE("NB", "Norvegese"),
-    OLANDESE("NL", "Olandese"),
-    POLACCO("PL", "Polacco"),
-    PORTOGHESE("PT-PT", "Portoghese"),
-    RUMENO("RO", "Rumeno"),
-    RUSSO("RU", "Russo"),
-    SLOVACCO("SK", "Slovacco"),
-    SLOVENO("SL", "Sloveno"),
-    SPAGNOLO("ES", "Spagnolo"),
-    SVEDESE("SV", "Svedese"),
-    TEDESCO("DE", "Tedesco"),
-    TURCO("TR", "Turco"),
-    UCRAINO("UK", "Ucraino"),
-    UNGARO("HU", "Ungaro");
+    ARABIAN("AR", "Arabian"),
+    BULGARIAN("BG", "Bulgarian"),
+    CZECH("CS", "Czech"),
+    CHINESE("ZH-HANT", "Chinese"),
+    KOREAN("KO", "Korean"),
+    DANISH("DA", "Danish"),
+    ESTONIAN("ET", "Estonian"),
+    FINNISH("FI", "Finnish"),
+    FRENCH("FR", "French"),
+    JAPANESE("JA", "Japanese"),
+    GREEK("EL", "Greek"),
+    INDONESIAN("ID", "Indonesian"),
+    ENGLISH("EN-GB", "English"),
+    ITALIAN("IT", "Italian"),
+    LATVIAN("LV", "Latvian"),
+    LITHUANIAN("LT", "Lithuanian"),
+    NORWEGIAN("NB", "Norwegian"),
+    DUTCH("NL", "Dutch"),
+    POLISH("PL", "Polish"),
+    PORTUGUESE("PT-PT", "Portuguese"),
+    ROMANIAN("RO", "Romanian"),
+    RUSSIAN("RU", "Russian"),
+    SLOVAK("SK", "Slovak"),
+    SLOVENIAN("SL", "Slovenian"),
+    SPANISH("ES", "Spanish"),
+    SWEDISH("SV", "Swedish"),
+    GERMAN("DE", "German"),
+    TURKISH("TR", "Turkish"),
+    UKRAINIAN("UK", "Ukrainian"),
+    HUNGARIAN("HU", "Hungarian");
 
 
     public final String idLanguage;
     public final String description;
-
-
 
 
 }
